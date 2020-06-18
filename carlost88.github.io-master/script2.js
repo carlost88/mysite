0673e589aaf53e0cf72ffd88f60206f3c78c5638
carlost88 <48337344+carlost88@users.noreply.github.com>
2020-06-18 07:13:19 -0700

    cdscsd

---
+++ carlost88.github.io-master/script2.js
@@ -0,0 +1,7 @@
+let login = document.getElementById("register-submit");
+
+const thanks = () => {
+	alert("Thank you for registering!");
+}
+
+login.addEventListener("click", thanks);
---
+++ carlost88.github.io-master/script3.js
@@ -0,0 +1,36 @@
+imgHolder = document.querySelector('.babyBull');
+coordinates = document.querySelector('h2');
+title = document.querySelector('h1');
+disco = document.querySelector('.disco')
+
+const bull = document.querySelector('#bull');
+const bull2 = document.createElement('img'); 
+bull2.src ="bull2.png";
+
+let displayedImage = false;
+
+const disappear = (e) => {
+    displayedImage = !displayedImage;
+    if (displayedImage){
+        imgHolder.replaceChild(bull2, bull);
+    } else {
+        imgHolder.replaceChild(bull, bull2);
+    }
+}
+
+const getCoords = (e) => {
+	coordinates.textContent = `X: ${e.offsetX} Y: ${e.offsetY}`;
+}
+
+const colorChange = (e) => {
+	title.style.color = `rgb(${e.offsetX}, 45, ${e.offsetY})`;
+}
+
+const colorChangeBlack = (e) => {
+	title.style.color = 'black';
+}
+
+disco.addEventListener('mousemove', colorChange);
+disco.addEventListener('mouseleave', colorChangeBlack);
+imgHolder.addEventListener('click', disappear);
+document.querySelector('body').addEventListener('mousemove', getCoords);
